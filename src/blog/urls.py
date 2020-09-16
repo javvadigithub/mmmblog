91a5d4acc7860193450b987eb7daa9d6ccdcbfa9
javvadigithub <j_srikanth@outlook.com>
2020-09-16 22:20:59 +0530

    Front End and News Letter Updated

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from .views import index,blog
+from posts.views import index,blog
 
 
 
